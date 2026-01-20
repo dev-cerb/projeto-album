0a7372f273c3dc0330266c6d1f7c578cef910860
Carlos Barreto <contato.cerb@outlook.com>
2026-01-20 01:52:25 -0300

    `fixx:` album controller

--- backend/routes/albumRoutes.js
+++ backend/routes/albumRoutes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import AuthMiddleware from "../middlewares/authMiddleware.js";
-import { listAlbums, createAlbum, updateAlbum, deleteAlbum } from "../controllers/albumController.js";
+import {
+  listAlbums,
+  createAlbum,
+  updateAlbum,
+  deleteAlbum,
+  getAlbum,
+} from "../controllers/albumController.js";
 
 const router = Router();
 
-router.get('/', AuthMiddleware, listAlbums)
-router.post('/', AuthMiddleware, createAlbum)
-router.put('/:id', AuthMiddleware, updateAlbum)
-router.delete('/:id', AuthMiddleware, deleteAlbum)
+router.get("/", AuthMiddleware, listAlbums);
+router.get("/:id", AuthMiddleware, getAlbum);
+router.post("/", AuthMiddleware, createAlbum);
+router.put("/:id", AuthMiddleware, updateAlbum);
+router.delete("/:id", AuthMiddleware, deleteAlbum);
 
 export default router;
